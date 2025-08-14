dc46049d7ea6db07336775549c2612dc15c3c128
kik4817 <kik4817@naver.com>
2025-08-14 17:49:18 +0900

    (2025-08-14) 3D 캐릭터 모델링 사용 및 애니메이션 적용시키기

---
+++ UnityBasic_3D/UnityBasic_3D/Assets/Scripts/Char.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Char : MonoBehaviour
+{
+    Animator animator;
+    float speed;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        //if(Keyboard.current.tKey.wasPressedThisFrame)
+        //{
+           
+        //}
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            speed += Time.deltaTime;
+            animator.SetFloat("speed", speed);
+        }
+        else if (Input.GetKey(KeyCode.T))
+        {
+            speed =0;
+        }
+    }
+}
